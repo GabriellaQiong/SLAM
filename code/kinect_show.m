4c604094f001209597ce9727da41de2b7a533f32
Qiong Wang <wangqiong0516@gmail.com>
2014-03-17 23:23:14 -0400

    Delete kinect_show.m

--- code/kinect_show.m
+++
@@ -1,5 +0,0 @@
-k = load('kinect20_unpacked_0mat')
-figure; plot(1:100, k.rgb_ts, 1:100, k.depth_ts) % time series
-figure; imshow(squeeze(k.rgb(1,:,:,:))/255) % first RGB frame
-figure; imshow(squeeze(k.depth(1,:,:))/2048) % first depth frame
-
